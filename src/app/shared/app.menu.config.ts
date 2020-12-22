86ec4fa399c4ce0f439ca3deeba0446306dcb335
nlodeiro <noelia.fraga@campusdual.com>
2020-12-22 16:02:52 +0100

    Creacción acceso directo Rooms

--- src/app/shared/app.menu.config.ts
+++ src/app/shared/app.menu.config.ts
@@ -2,5 +2,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
+  { id: 'rooms', name: 'ROOMS', icon: 'view_list', route: '/main/rooms'},
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
